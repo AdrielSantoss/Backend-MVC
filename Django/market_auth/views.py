ba192195948ac5bcc2bed20b0a62fc5ad50efbae
Adriel dos Santos <adrieldossantos7@gmail.com>
2024-01-16 21:06:28 -0300

    Django/Login: Adiciona inicio do do card de login.

--- Django/market_auth/views.py
+++ Django/market_auth/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def login(request):
-    return render(request, 'market_auth/pages/login.html')
+    return render(request, 'market_auth/pages/login.html', {
+        "title": "Login"
+    })
